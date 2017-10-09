a8c655bc02064ea4f62b8e4d6de7e3a300df0571
yashasingh <yashasingh7@gmail.com>
2017-10-09 12:38:14 +0530

    minor fixes

--- App/views.py
+++ App/views.py
@@ -31,7 +31,7 @@ def main(request):
 
         if(response.status_code != 200):
             return render(request,
-                          'Microtask1/index.html',
+                          'App/index.html',
                           {"error":'HTTP response {}'.format(str(response.status_code)),
                            "response": "Invalid username",
                            "code": 1}
@@ -41,19 +41,19 @@ def main(request):
             try:
                 if lst['error']:
                     return render(request,
-                                 'Microtask1/index.html',
+                                 'App/index.html',
                                  {"error":lst['error']['info']})
             except:
                 # Contribution data extracted
                 sub_lst = lst['query']['usercontribs']
                 if len(sub_lst) == 0:
                     return render(request,
-                                 'Microtask1/index.html',
+                                 'App/index.html',
                                  {"error":"No Edits found for username {}".format(username)})
 
             # Send contribution data for display
             return render(request,
-                         'Microtask1/index.html',
+                         'App/index.html',
                          {"contribution":sub_lst, "username":username})
     else:
-        return render(request, 'Microtask1/index.html')
+        return render(request, 'App/index.html')
