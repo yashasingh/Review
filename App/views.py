c9e8057d2a5317d8d60b1958c4e4ece29d877d1a
yashasingh <yashasingh7@gmail.com>
2017-10-09 12:07:21 +0530

    conflict resolved

--- App/views.py
+++ App/views.py
@@ -5,7 +5,28 @@ import json
 import requests
 import toolforge
 
-<<<<<<< HEAD
+def main(request):
+    if(request.method=='POST'):
+        usr = request.POST['username']  #Here, we get the username fetched from html page
+        URL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=usercontribs&ucuser="
+        URL = URL + usr
+        r = requests.get(URL)  #Recieved data in json-format
+        if(r.status_code != 200):
+            return(render(request,'App/index.html',{"error":'HTTP response ' + str(r.status_code)}))
+        else:
+            lst = json.loads(r.text)
+            try:    
+                if(lst['error']):
+                    return(render(request,'App/index.html',{"error":lst['error']['info']}))
+            except:   
+                sub_lst = lst['query']['usercontribs'] #Contribution data extracted
+                if(len(sub_lst)==0):
+                    return(render(request,'App/index.html',{"error":"No Edits found"}))
+            return(render(request,'App/index.html',{"contribution":sub_lst, "username":usr}))  #Here I send contribution for display on index.html page
+    else:
+        return(render(request,'App/index.html',{}))
+
+
 def main2(request):
     if(request.method=='POST'):
         username = request.POST['username']
@@ -35,25 +56,4 @@ def main2(request):
  
     else:
         return(render(request, 'App/task2.html',{}))
-=======
-def main(request):
-    if(request.method=='POST'):
-        usr = request.POST['username']  #Here, we get the username fetched from html page
-        URL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=usercontribs&ucuser="
-        URL = URL + usr
-        r = requests.get(URL)  #Recieved data in json-format
-        if(r.status_code != 200):
-            return(render(request,'App/index.html',{"error":'HTTP response ' + str(r.status_code)}))
-        else:
-            lst = json.loads(r.text)
-            try:    
-                if(lst['error']):
-                    return(render(request,'App/index.html',{"error":lst['error']['info']}))
-            except:   
-                sub_lst = lst['query']['usercontribs'] #Contribution data extracted
-                if(len(sub_lst)==0):
-                    return(render(request,'App/index.html',{"error":"No Edits found"}))
-            return(render(request,'App/index.html',{"contribution":sub_lst, "username":usr}))  #Here I send contribution for display on index.html page
-    else:
-        return(render(request,'App/index.html',{}))
->>>>>>> a613a3594c61bb8763f52a83420d06ff03422567
+
