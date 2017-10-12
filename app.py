7f5fbe8d4c454f6020fe6bb62f17038bfa76825b
yashasingh <yashasingh7@gmail.com>
2017-10-12 12:26:46 +0530

    minor fix

--- app.py
+++ app.py
@@ -5,7 +5,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Project.settings")
 
 app = get_wsgi_application()
-<<<<<<< HEAD
-=======
-
->>>>>>> 30b27c9bedb37225be757d8f3100954b54cc3a08
