4c4fcab05e5ddedec8ef7159350d6acfd749c43a
yashasingh <yashasingh7@gmail.com>
2017-11-09 00:16:05 +0530

    minor fixes

--- App/views.py
+++ App/views.py
@@ -121,13 +121,12 @@ def get_article_view_count(request):
     """
     Display analysis of last 5 edits made by user.
     """
-    if(request.method=='POST'):
+    if request.method=='POST':
         # Here, we get the username
         username = request.POST['username']
         # if username is submitted blank 
         if not username:
             return render(request, "App/task4.html", {"error": "Please enter a username"})
-
         # if username is not blank
         details = list()
         parameters = {'action':'query',
@@ -135,60 +134,51 @@ def get_article_view_count(request):
                       'list':'usercontribs',
                       'uclimit':'5',
                       'ucuser':username}
-        url_parameters = urllib.parse.urlencode(parameters)
-        url1 = 'https://en.wikipedia.org/w/api.php?' + url_parameters
-        
+        url_base = 'https://en.wikipedia.org/w/api.php?'
         # Recieved data in json-format
-        response = requests.get(url1)
- 
+        response = requests.get(url_base, params = parameters)
         if response.status_code == 200:
-            lst = json.loads(response.text)
             try:
-                # if username contains invalid value
-                if lst['error']:
-                    return render(request,
-                                  'App/task4.html',
-                                  {"error":lst['error']['info']}
-                                 )
-            except:
                 # Edit data extracted
                 edits_data = json.loads(response.text)
-                articles = [i for i in edits_data['query']['usercontribs']]
+                if 'error' in edits_data:
+                    raise Error(edits_data['error']['info'])
+                contributions = [i for i in edits_data['query']['usercontribs']]
                 # if user has no edits 
-                if len(articles) == 0:
-                    return render(request,
-                                  'App/task4.html',
-                                  {"error": "0 edits found for username {}".format(username)})
-
-                for i in articles:
-                    url2 = "https://ores.wikimedia.org/v3/scores/enwiki/" + str(i['revid'])
+                if len(contributions) == 0:
+                    raise Error("0 edits found for username {}".format(username))
+                for articles in contributions:
+                    url_ores = "https://ores.wikimedia.org/v3/scores/enwiki/{}".format(str(articles['revid']))
                     # recieve ORES response
-                    response2 = requests.get(url2)
+                    response2 = requests.get(url_ores)
                     if response2.status_code != 200:
-                        return(render(request,
-                                      'App/tesk4.html',
-                                      {"error": "ORES failed to return response"}))
-
+                        raise Error("ORES failed to return response")
                     # if successfull response from ORES
                     data = json.loads(response2.text)
-                    analysis = data['enwiki']['scores'][str(i['revid'])]
+                    analysis = data['enwiki']['scores'][str(articles['revid'])]
                     predictions = {'goodfaith': analysis['goodfaith']['score']['prediction'],
-                                   'reverted': analysis['reverted']['score']['prediction'],
+                                   #'reverted': analysis['reverted']['score']['prediction'],
                                    'damaging': analysis['damaging']['score']['prediction'],
                                    'draftquality': analysis['draftquality']['score']['prediction'],
                                    'quality': analysis['wp10']['score']['prediction'],
-                                   'title': i['title'],
-                                   'comment': i['comment'],
-                                   'timestamp': i['timestamp'],
-                                   'revid': i['revid'],
-                                   'user': i['user']}
+                                   'title': articles['title'],
+                                   'comment': articles['comment'],
+                                   'timestamp': articles['timestamp'],
+                                   'revid': articles['revid'],
+                                   'user': articles['user']}
                     details.append(predictions)
-        context = {'articles': details}
-        return(render(request,'App/task4.html',context))
-
+            except Exception as err:
+                return(render(request,
+                              'App/task4.html',
+                              {'error': err}))
+            context = {'articles': details}
+            return render(request, 'App/task4.html', context)
+        else:
+            return render(request,
+                          'App/index.html',
+                          {"error": 'Exit code {}'.format(str(response.status_code))})
     else:
-        return(render(request,'App/task4.html',{}))
-
+        return render(request,'App/task4.html',{})
 
 def task5(request):
     if request.method=='POST':
